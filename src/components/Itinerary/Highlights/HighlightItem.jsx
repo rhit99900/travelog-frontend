115ecd6478ed6b07fc53d95c8fdd6f69417b9f2d
unknown <chris.reejoy90@gmail.com>
2020-10-13 21:20:24 +0530

    itinerary UI page changes

---
+++ src/components/Itinerary/Highlights/HighlightItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HightlightItem = () => {
+  return (
+    <div className="itineraryHighlightsItem">
+      <div className="itineraryHighlightsImage viewed">
+        <img src="https://picsum.photos/id/689/80/80"/>          
+      </div>    
+    </div>
+  )
+}
+
+export default HightlightItem
