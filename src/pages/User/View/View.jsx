d97720acedd27ac9725f706155e4a3a6216f55c0
unknown <chris.reejoy90@gmail.com>
2020-10-25 13:53:58 +0530

    style changes

--- src/pages/User/View/View.jsx
+++ src/pages/User/View/View.jsx
@@ -21,13 +21,18 @@ import { searchOutline, personSharp, chatboxOutline, heartOutline, heartSharp} f
 
 import './View.css';
 const slideOpts = {
-  initialSlide: 1,
-  speed: 400
+  initialSlide: 0,
+  speed: 400,
+   spaceBetween: 100,
+  grabCursor: true,
+   effect: 'fade'
 };
 
 function next(slide, index) {
     slide.slideTo(index)
 };
+
+
 
 const View = () => {
   return (
@@ -72,96 +77,33 @@ const View = () => {
 	     		</IonRow>
 	     </IonGrid>
 	
-	</IonHeader>	  
-
-      <IonSlides className="IonSlides" pager={true}>
-        <IonSlide >
-         <div line="none" className="mainDiv ion-margin-bottom" >
+	</IonHeader>	 
+  
+        <IonSlides id="slides" className="IonSlides" pager={true} options={slideOpts}>
+       
+  {[1,2,3,4].map((item, index) => (      
+        <IonSlide>
+         <div line="none" className="mainDiv" >
          	<p className="headerContent">
 	         	<span className="leftContent" slot="left">08:45 AM</span>
 	         	<span className="content text-right" slot="left">Notre Dam cathedral</span>	   
 	         </p>      
-       		<img src="https://picsum.photos/536/354" className="slideImg"/>
+       		<img height="100%" src="https://picsum.photos/536/354" className="slideImg"/>
          </div> 
         <IonItem lines="none">
          <IonLabel className="leftContent">    
            <span className="contentDivCommentTxt">256</span>  <IonIcon className="smallIcons" slot="end" icon={chatboxOutline}> 98</IonIcon>             
      	 	&nbsp;<span className="contentDivCommentTxt ion-margin-left">576</span>  <IonIcon className="smallIcons" color="primary" slot="end" icon={heartSharp}> 98</IonIcon>            
-     	</IonLabel>
+     	  </IonLabel>         
      	   </IonItem>
-		<IonItem lines="none" className="text-left contextTxt">
-     
-     	First stop the iconic Notre Dame cathedral, we got here nice and early, best to avoid the ilnes.
-     	This allowed us to climp the towers to enjoy Paris from a gargoyale's panormic perspective and to admire the gargoyales and grotesques themselves.
-     	
-       </IonItem>
-        </IonSlide>
 
-        <IonSlide >
-         <div line="none" className="mainDiv ion-margin-bottom" >
-         	<p className="headerContent">
-	         	<span className="leftContent" slot="left">08:45 AM</span>
-	         	<span className="content text-right" slot="left">Notre Dam cathedral</span>	   
-	         </p>      
-       		<img src="https://picsum.photos/536/355" className="slideImg"/>
-         </div> 
-        <IonItem lines="none">
-         <IonLabel className="leftContent">    
-           <span className="contentDivCommentTxt">256</span>  <IonIcon className="smallIcons" slot="end" icon={chatboxOutline}> 98</IonIcon>             
-     	 	&nbsp;<span className="contentDivCommentTxt ion-margin-left">576</span>  <IonIcon className="smallIcons" color="primary" slot="end" icon={heartSharp}> 98</IonIcon>            
-     	</IonLabel>
-     	   </IonItem>
-		<IonItem lines="none" className="text-left contextTxt">
-     
-     	First stop the iconic Notre Dame cathedral, we got here nice and early, best to avoid the ilnes.
-     	This allowed us to climp the towers to enjoy Paris from a gargoyale's panormic perspective and to admire the gargoyales and grotesques themselves.
-     	
-       </IonItem>
+    		<IonItem lines="none" className="text-left contextTxt">         
+         	First stop the iconic Notre Dame cathedral, we got here nice and early, best to avoid the ilnes.
+         	This allowed us to climp the towers to enjoy Paris from a gargoyale's panormic perspective and to admire the gargoyales and grotesques themselves.        	
+         </IonItem>
         </IonSlide>
-
-        <IonSlide >
-         <div line="none" className="mainDiv ion-margin-bottom" >
-         	<p className="headerContent">
-	         	<span className="leftContent" slot="left">08:45 AM</span>
-	         	<span className="content text-right" slot="left">Notre Dam cathedral</span>	   
-	         </p>      
-       		<img src="https://picsum.photos/536/354" className="slideImg"/>
-         </div> 
-        <IonItem lines="none">
-         <IonLabel className="leftContent">    
-           <span className="contentDivCommentTxt">256</span>  <IonIcon className="smallIcons" slot="end" icon={chatboxOutline}> 98</IonIcon>             
-     	 	&nbsp;<span className="contentDivCommentTxt ion-margin-left">576</span>  <IonIcon className="smallIcons" color="primary" slot="end" icon={heartSharp}> 98</IonIcon>            
-     	</IonLabel>
-     	   </IonItem>
-		<IonItem lines="none" className="text-left contextTxt">
-     
-     	First stop the iconic Notre Dame cathedral, we got here nice and early, best to avoid the ilnes.
-     	This allowed us to climp the towers to enjoy Paris from a gargoyale's panormic perspective and to admire the gargoyales and grotesques themselves.
-     	
-       </IonItem>
-        </IonSlide>
-
-        <IonSlide >
-         <div line="none" className="mainDiv ion-margin-bottom" >
-         	<p className="headerContent">
-	         	<span className="leftContent" slot="left">08:45 AM</span>
-	         	<span className="content text-right" slot="left">Notre Dam cathedral</span>	   
-	         </p>      
-       		<img src="https://picsum.photos/536/356" className="slideImg"/>
-         </div> 
-        <IonItem lines="none">
-         <IonLabel className="leftContent">    
-           <span className="contentDivCommentTxt">256</span>  <IonIcon className="smallIcons" slot="end" icon={chatboxOutline}> 98</IonIcon>             
-     	 	&nbsp;<span className="contentDivCommentTxt ion-margin-left">576</span>  <IonIcon className="smallIcons" color="primary" slot="end" icon={heartSharp}> 98</IonIcon>            
-     	</IonLabel>
-     	   </IonItem>
-		<IonItem lines="none" className="text-left contextTxt">
-     
-     	First stop the iconic Notre Dame cathedral, we got here nice and early, best to avoid the ilnes.
-     	This allowed us to climp the towers to enjoy Paris from a gargoyale's panormic perspective and to admire the gargoyales and grotesques themselves.
-     	
-       </IonItem>
-        </IonSlide>
+ ))};       
+       
 
       </IonSlides>
     </IonContent>
